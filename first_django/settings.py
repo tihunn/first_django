66af05b03e6f4e21f8d991005a2a16278af9c51c
tihunn <tihon.volkov@mail.ru>
2023-09-29 15:01:13 +0300

    filled templates and static

--- first_django/settings.py
+++ first_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^1ox7z12l&jsd-*tichrjphcikr&v0=w0tl(hk1(@i6t%5$5&x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- women/views.py
+++ women/views.py
@@ -3,27 +3,19 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 from women.models import Women
 
-menu = ["About site, Add article", "Contact us", "Log in"]
+menu = ["About site", "Add article", "Contact us", "Log in"]
 
 
 # Create your views here.
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {"title": "Main page", "menu": menu, "posts": posts})
+    return render(request,
+                  'women/index.html',
+                  {"title": "Main page", "menu": menu, "posts": posts})
 
 
 def about(request):
     return render(request, 'women/about.html', {"title": "About site", "menu": menu})
-
-
-def categories(request, catid):
-    return HttpResponse(f"<h1>Catalog page</h1><p>{catid}</p>")
-
-
-def archive(request, year):
-    if 1970 < int(year) > 2023:
-        raise Http404()
-    return HttpResponse(f"<h1>Archive for year</h1><p>{year}</p>")
 
 
 def page_not_found(request, exception):
